2ef5048fe37240f18a25b849ed4e7fd7e19f7283
rayan kashif <rayankashif71@hotmail.com>
2025-02-02 16:35:59 -0600

    push command implemented

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import sys
-import select
+import os
+
 
 # get the login credentials from the command line arguments
 try:
@@ -17,18 +18,18 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect to the server
 try:
-    client_socket.connect((hostname,port))
+    client_socket.connect((hostname, port))
     print(f"Connected to the {hostname} at port {port}")
 except socket.error as e:
     print(f"Error connecting to server: {e}")
     sys.exit(1)
 
-#login authentication
+# login authentication
 # Send the LOGIN command to the server
-client_socket.sendall(f"LOGIN {username}\n".encode('utf-8'))
+client_socket.sendall(f"LOGIN {username}\n".encode("utf-8"))
 
 # Receive the server's response
-response = client_socket.recv(1024).decode('utf-8').strip()
+response = client_socket.recv(1024).decode("utf-8").strip()
 print("Server response:", response)
 
 if response.strip() == "LOGIN SUCCESSFUL":
@@ -36,18 +37,64 @@ if response.strip() == "LOGIN SUCCESSFUL":
 
     # After successful login, you can keep the connection open and interact with the server
     while True:
+
         # Example: Requesting a list of files or any other command
-        command = input("Enter command (e.g., LIST, UPLOAD, DOWNLOAD, or EXIT): ")
+        user_command = input("Enter command options (e.g., PUSH, LIST, GET, or EXIT): ")
 
-        if command == "EXIT":
-            client_socket.sendall("EXIT\n".encode('utf-8'))
-            break  # Exit the loop and close the connection
-        
-        client_socket.sendall(command.encode('utf-8'))
+        # ******* Handle push command, parse and send the file and its content ********
+        if user_command.upper().startswith("PUSH "):
+            try:
+                _, filename = user_command.split(" ", 1)  # getting the filename
 
-        # Receive the server's response
-        response = client_socket.recv(1024).decode('utf-8').strip()
-        print(f"Server response: {response}")
+                # Check if file exists locally
+                if not os.path.exists(filename):
+                    print(f"File {filename} not found.")
+                    continue
 
-# Close the client socket after exiting the loop
+                # send the push command
+                client_socket.sendall(f"PUSH {filename}\n".encode("utf-8"))
+
+                total_sent = 0  # size of data sent
+
+                # open the file read binary and send the command to the server
+                with open(filename, "rb") as file_sent:
+                    while data := file_sent.read(4096):
+                        client_socket.sendall(data)
+
+                # message for server to know that the whole is transferred
+                client_socket.sendall(b"EOF\n")
+
+                print(f"{filename} sent successfully.")
+
+                response = (
+                    client_socket.recv(1024).decode("utf-8").strip()
+                )  # Get server response
+                print(f"Server response: {response}")
+
+            except FileNotFoundError:
+                print("file {filename} not found.")
+                continue
+
+            except Exception:
+                print("Error Try Again.")
+                continue
+
+        # ******* Handle GET command ********
+        # ******* Handle LIST command ********
+        # ******* Handle DELETE command ********
+
+        elif user_command == "EXIT":
+
+            client_socket.sendall("EXIT\n".encode("utf-8"))  # Send exit command
+
+            response = (
+                client_socket.recv(1024).decode("utf-8").strip()
+            )  # Get server response
+            print(f"🔹 Server response: {response}")
+
+            print(" Closing connection...")
+            break  # Exit the loop
+
+# Close connection after exiting the loop
 client_socket.close()
+print(" Connection closed.")
--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 import socket
 import sys
 import select
+import os
+import time
+
+# files on the server side
+SERVER_FILES = "server_files"
+METADATA_FILE = os.path.join(
+    SERVER_FILES, "metadata.txt"
+)  # text file to store metadata
 
 try:
-    hostname = '' # Hostname (can be '' or 'localhost' for local machine)
+    hostname = ""  # Hostname (can be '' or 'localhost' for local machine)
     port = int(sys.argv[1])  # Port number
 except:
     print("Usage: python filename.py port")
@@ -22,71 +30,139 @@ except socket.error as e:
 
 server_socket.listen()
 
-# maintaining a socket list and clients 
+# maintaining a socket list and clients
 sockets_list = [server_socket]
 clients = {}  # Dictionary to map sockets to usernames
 
+# Ensure server_files directory exists
+if not os.path.exists(SERVER_FILES):
+    os.makedirs(SERVER_FILES)
+    print(f"Created directory: {SERVER_FILES}")
+
+# Ensure metadata.txt exists or create it with a header if not
+if not os.path.exists(METADATA_FILE):
+    with open(METADATA_FILE, "w") as meta_file:
+        meta_file.write("username,filename,size_MB,timestamp\n")
+    print(f"Created metadata file: {METADATA_FILE}")
+
 try:
- # main loop with the select statement
- while True:
+    # main loop with the select statement
+    while True:
 
-    #select the socket from the readable sockets using a select statement, handles multiple clients
-    readable_sockets, writable_sockets, exceptional_sockets = select.select(sockets_list, [], [])
+        # select the socket from the readable sockets using a select statement, handles multiple clients
+        readable_sockets, writable_sockets, exceptional_sockets = select.select(
+            sockets_list, [], []
+        )
 
-    # for each socket in the readable_sockets list check the following conditions
-    for r in readable_sockets:
-        
-        if r == server_socket: # if r is a server socket establish a new connection
-            connection_socket , client_address = server_socket.accept()
-            print(f"New Client Connected: {client_address}")
-            sockets_list.append(connection_socket) # add new client to the sockets_list
-            
-        else: #handle client messages
-            try:
-                message = r.recv(1024).decode('utf-8').strip()
-                
-                #empty message
-                if not message:  # Client disconnected
-                    print(f"Client {r.getpeername()} disconnected")
-                    sockets_list.remove(r)
-                    r.close()
-                    continue  # Skip further processing for this socket
-                
-                #handle client authentication
-                if r not in clients:
-                        
-                      if message.startswith("LOGIN "):  # If login command received
-                        username = message.split(" ", 1)[1]
-                        clients[r] = username 
-                        response = "LOGIN SUCCESSFUL\n"
-                        r.sendall(response.encode('utf-8'))  # Send response to client
-                        
-                      else:#Invalid user since there is no login command sent from client
-                            r.sendall("INVALID USER\n".encode('utf-8'))
-                            sockets_list.remove(r)
-                            r.close()
-                                            
-                else:  # If user is already authenticated, now im handling further commands
-                    username = clients[r]
-                    print(f"Command from {username}: {message}")
-                    if message == "EXIT":
-                        r.sendall("Goodbye!\n".encode('utf-8'))
+        # for each socket in the readable_sockets list check the following conditions
+        for r in readable_sockets:
+
+            if r == server_socket:  # if r is a server socket establish a new connection
+                connection_socket, client_address = server_socket.accept()
+                print(f"New Client Connected: {client_address}")
+                # add new client to the sockets_list
+                sockets_list.append(connection_socket)
+
+            else:  # handle client messages
+                try:
+                    message = r.recv(1024).decode("utf-8").strip()
+
+                    # empty message
+                    if not message:  # Client disconnected
+                        print(f"Client {r.getpeername()} disconnected")
                         sockets_list.remove(r)
                         r.close()
-                    else:
-                        r.sendall(f"Unknown command: {message}\n".encode('utf-8'))
-            
-            except Exception as e:
-                print(f"Error with client: {e}")
-                if r in clients:
-                    del clients[r]
-                sockets_list.remove(r)
-                r.close()
-                
-except KeyboardInterrupt:
- print("\nServer Sutting")
- for sock in sockets_list:
-  sock.close()
+                        continue  # Skip further processing for this socket
 
- server_socket.close()
- sys.exit(0)
+                    # handle client authentication
+                    if r not in clients:
+
+                        # If login command received
+                        if message.startswith("LOGIN "):
+                            username = message.split(" ", 1)[1]
+                            clients[r] = username
+                            response = "LOGIN SUCCESSFUL\n"
+                            # Send response to client
+                            r.sendall(response.encode("utf-8"))
+
+                        else:  # Invalid user since there is no login command sent from client
+                            r.sendall("INVALID USER\n".encode("utf-8"))
+                            sockets_list.remove(r)
+                            r.close()
+
+                    else:  # If user is already authenticated, now im handling further commands
+                        username = clients[r]
+
+                        print(f"Command from {username}: {message}")
+
+                        # ******Handle PUSH Command**********
+                        if message.upper().startswith("PUSH "):
+                            try:
+                                _, filename = message.split(" ", 1)
+                                file_location = os.path.join(SERVER_FILES, filename)
+
+                                with open(file_location, "wb") as file_received:
+                                    while True:
+                                        data = r.recv(4096)
+                                        if not data:
+                                            break  # Stop if client disconnects
+
+                                        # Check if EOF is reached
+                                        if data.endswith(b"EOF\n"):
+                                            file_received.write(
+                                                data[:-4]
+                                            )  # Remove EOF marker
+                                            break
+                                        else:
+                                            file_received.write(data)
+
+                                #
+                                file_received.close()
+
+                                #  Calculate file size in bytes
+                                file_size = os.path.getsize(file_location)
+
+                                print(f"{file_size}")
+
+                                #  Get timestamp
+                                timestamp = time.strftime(
+                                    "%Y-%m-%d %H:%M:%S", time.localtime()
+                                )
+                                print(f"{timestamp}")
+
+                                #  Store metadata in the metadata file
+                                with open(METADATA_FILE, "a") as meta_file:
+                                    meta_file.write(
+                                        f"{clients[r]},{filename},{file_size},{timestamp}\n"
+                                    )
+                                    meta_file.close()
+
+                                print(f"File '{filename}' uploaded successfully.")
+                                r.sendall(
+                                    f"File '{filename}' uploaded successfully.\n".encode(
+                                        "utf-8"
+                                    )
+                                )
+
+                            except Exception as e:
+                                print(f"Error receiving file: {e}")
+                                r.sendall(
+                                    f"Error receiving file: {e}\n".encode("utf-8")
+                                )
+
+                        else:
+                            r.sendall(f"Unknown command: {message}\n".encode("utf-8"))
+
+                except Exception as e:
+                    print(f"Error with client: {e}")
+                    if r in clients:
+                        del clients[r]
+                    sockets_list.remove(r)
+                    r.close()
+
+except KeyboardInterrupt:
+    print("\nServer Sutting")
+    for sock in sockets_list:
+        sock.close()
+        server_socket.close()
+        sys.exit(0)
